eee7175cc9d426ea1a878679afbb16c47bb9a203
lunalopez0125@gmail.com <lunalopez0125@gmail.com>
2025-11-25 10:47:37 -0300

    Initialize project

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// ============================================
+// TAREA: Crear una To Do App
+// ============================================
+//
+// REQUISITOS:
+// 1. Implementar un CRUD completo:
+//      - Crear
+//      - Leer
+//      - Actualizar
+//      - Eliminar
+//
+// 2. Guardad los datps localmente (LocalStorage)
+// 3. Diseño interfaz de forma clara y amigable.
+//
+// ============================================
